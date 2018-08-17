d5b323d560bda1c68134438903a81f97f9b7cdf7
杨凌霄 <ylx@disanbo.com>
2018-08-17 13:39:20 +0800

    first commit

---
+++ src/main/java/com/fuzamei/MysqlPagequeryToRedisApplication.java
@@ -0,0 +1,12 @@
+package com.fuzamei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlPagequeryToRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlPagequeryToRedisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fuzamei/MysqlPagequeryToRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fuzamei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlPagequeryToRedisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
