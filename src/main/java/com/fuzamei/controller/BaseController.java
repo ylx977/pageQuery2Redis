45a25576dae64b9f84cd72d91fac6b3bd69a3f5c
杨凌霄 <ylx@disanbo.com>
2018-11-29 09:58:07 +0800

    发现一个狗屁注解，删掉

--- src/main/java/com/fuzamei/controller/BaseController.java
+++ src/main/java/com/fuzamei/controller/BaseController.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (C) 2016  HangZhou YuShi Technology Co.Ltd  Holdings Ltd. All rights reserved
- *
- * 本代码版权归杭州宇石科技所有，且受到相关的法律保护。
- * 没有经过版权所有者的书面同意，
- * 任何其他个人或组织均不得以任何形式将本文件或本文件的部分代码用于其他商业用途。
- *
- */
 package com.fuzamei.controller;
 
 
